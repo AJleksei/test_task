5457f5226f82753f933fb5d0807e2b784b28aa01
AJleksei <AJleksei@ukr.net>
2016-03-14 23:56:33 +0200

    united method of encode and decode in one

--- caesar_cipher/views.py
+++ caesar_cipher/views.py
@@ -13,12 +13,38 @@ def index(request):
         shift = int(request.POST['shift'])
         res = ''
         if encode in request.POST:
-            res = encode_caesar(text, shift)
+            res = automatic_encode_decode_caesar(text, shift, encode)
         if decode in request.POST:
-            res = decode_caesar(text, shift)
+            res = automatic_encode_decode_caesar(text, shift, decode)
         args['res'] = res
 
     return render(request, 'index.html', args)
+
+
+def automatic_encode_decode_caesar(text, rot, action):
+    def action_symbol(symbol, rot, action):
+        alphabet = len(letters)
+        if action == decode:
+            res = ((letters.index(symbol) - rot + alphabet) % alphabet)
+            return res
+        if action == encode:
+            res = ((letters.index(symbol) + rot) % alphabet)
+            return res
+
+    result_text = ''
+    # В цикле проходим по всем символам
+    for symbol in text:
+        # Если символ есть в словаре шифруемых символов
+        if symbol in letters:
+            # Шифруем/дешифруем символ
+            symbol_index = action_symbol(symbol, rot, action)
+            result_text += letters[symbol_index]
+        # Если сивола нет в списке шифруемых символов
+        else:
+            # Записываем символ без изменений
+            result_text += symbol
+    return result_text
+
 
 
 def decode_caesar(encode_text, rot):
